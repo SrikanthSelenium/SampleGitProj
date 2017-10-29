269eb72050336da0a7c7faf6c387675ebabb5b1f
srikanthr2 <srikanth.chincholi@gmail.com>
2017-10-29 21:22:27 +0530

    added a file

---
+++ src/samplegithubproj/SampleClass.java
@@ -0,0 +1,11 @@
+package samplegithubproj;
+
+public class SampleClass {
+		
+	public static void main(String[] args) {
+		
+		System.out.println("Git Sample file");
+		System.out.println("Git Sample file with line 1");
+	}
+
+}
