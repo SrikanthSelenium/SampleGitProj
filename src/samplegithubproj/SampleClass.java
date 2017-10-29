de3b09cdde76c729e76d94219d676f2ee579b91a
= <=>
2017-10-29 22:41:01 +0530

    now new line committed

--- src/samplegithubproj/SampleClass.java
+++ src/samplegithubproj/SampleClass.java
@@ -5,8 +5,7 @@ public class SampleClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Git Sample file");
-		System.out.println("added a line for testing git branch b1");
-		System.out.println("added a line for testing git branch b1");
+		//commented new line
 	
 	}
 
